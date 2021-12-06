365fb98d7203dd96003c4c1c1f9f49af65188394
Elliot Shiu <sandbochs@users.noreply.github.com>
2021-12-06 18:46:32 +0900

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,107 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  WnD,
+  Approval,
+  ApprovalForAll,
+  DragonBurned,
+  DragonMinted,
+  DragonStolen,
+  OwnershipTransferred,
+  Paused,
+  Transfer,
+  Unpaused,
+  WizardBurned,
+  WizardMinted,
+  WizardStolen
+} from "../generated/WnD/WnD"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleApproval(event: Approval): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.PAID_TOKENS(...)
+  // - contract.aliases(...)
+  // - contract.balanceOf(...)
+  // - contract.existingCombinations(...)
+  // - contract.getApproved(...)
+  // - contract.getMaxTokens(...)
+  // - contract.getPaidTokens(...)
+  // - contract.getTokenTraits(...)
+  // - contract.getTokenWriteBlock(...)
+  // - contract.isApprovedForAll(...)
+  // - contract.isWizard(...)
+  // - contract.maxTokens(...)
+  // - contract.minted(...)
+  // - contract.name(...)
+  // - contract.owner(...)
+  // - contract.ownerOf(...)
+  // - contract.paused(...)
+  // - contract.randomizer(...)
+  // - contract.rarities(...)
+  // - contract.supportsInterface(...)
+  // - contract.symbol(...)
+  // - contract.tokenByIndex(...)
+  // - contract.tokenOfOwnerByIndex(...)
+  // - contract.tokenURI(...)
+  // - contract.totalSupply(...)
+  // - contract.tower(...)
+  // - contract.traits(...)
+}
+
+export function handleApprovalForAll(event: ApprovalForAll): void {}
+
+export function handleDragonBurned(event: DragonBurned): void {}
+
+export function handleDragonMinted(event: DragonMinted): void {}
+
+export function handleDragonStolen(event: DragonStolen): void {}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+
+export function handlePaused(event: Paused): void {}
+
+export function handleTransfer(event: Transfer): void {}
+
+export function handleUnpaused(event: Unpaused): void {}
+
+export function handleWizardBurned(event: WizardBurned): void {}
+
+export function handleWizardMinted(event: WizardMinted): void {}
+
+export function handleWizardStolen(event: WizardStolen): void {}
